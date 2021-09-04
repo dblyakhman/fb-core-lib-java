801b46c6557bea6d0e7a4a300e0ee5a27e7b6b88
Dmitry Blyakhman <33543531+dblyakhman@users.noreply.github.com>
2021-09-05 03:29:29 +0500

    Add files via upload

---
+++ src/exception/MailboxFullException.java
@@ -0,0 +1,8 @@
+//*****************************************************
+// Author: Dmitry Blyakhman, Yakov Starikov, 2000-2020
+//*****************************************************
+package exception;
+
+public class MailboxFullException extends FuncBlockException {
+
+}
