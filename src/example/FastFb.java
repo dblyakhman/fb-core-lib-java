21e5650d8a2aa158cc63b016e0b1ca83f69772e4
Dmitry Blyakhman <33543531+dblyakhman@users.noreply.github.com>
2021-09-05 11:14:03 +0500

    Update FastFb.java

--- src/example/FastFb.java
+++ src/example/FastFb.java
@@ -1,6 +1,3 @@
-//*****************************************************
-// Author: Dmitry Blyakhman, Yakov Starikov, 2000-2020
-//*****************************************************
 package example;
 
 import exception.FuncBlockException;
